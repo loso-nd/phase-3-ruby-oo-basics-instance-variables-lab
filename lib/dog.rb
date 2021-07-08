0123276ca209f3539744a85c5b9afdeef96a9cf7
Nahum <losond8@gmail.com>
2021-07-08 10:23:12 -0400

    Defined instance variables, distinguish instance varables from local, describe how instance variables give object attributes and properties.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,35 @@
-class Dog
+# class Dog
+#     def name=(dog_name)
+#         this_dogs_name = dog_name
+#     end
+
+
+#     def name
+#         this_dogs_name
+#     end
+# end
+
+
+#when we first run this code we an error bc #name method doesnt konw about the `this_dogs_aname` variable from the name=(dog_name) method. This would be an example of a local variable scope.
+
+class Dog 
+
+
+    def name=(dog_name)
+            #defines an instance variable @this_dogs_name
+            #setter method
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        #retrieves the value of an instance variable @this_dogs_name
+        #getter method
+        @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
